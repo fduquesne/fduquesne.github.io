ef38ff6637582f1caff35092ea5527a5f97c1903
fduquesne <fduquesne@icloud.com>
2020-04-18 18:35:03 +0200

    build : add some files that must be in the root of the `master` branch

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,12 @@ npm install
 npm run build
 npm run export
 
-# Push `out/` folder on the `master` branch
+# Add files that must be in the root of the `master` branch
 cd out/
+touch .nojekyll
+cp ../README.md .
+
+# Push `out/` folder content on the `master` branch
 git init
 git add .
 git commit -m 'build : deploy on github pages'
