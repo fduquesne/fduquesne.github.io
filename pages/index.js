5e018decb1c0b97dce633b0c3e18c8f0fbf46493
fduquesne <fduquesne@icloud.com>
2020-04-19 17:48:54 +0200

    feat(sidebar) : simulate snowfall

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ let Home = props => {
         <div className="container">
             <Head>
                 <title>Florian Duquesne</title>
+                <script src="./javascript/snowfall.js"></script>
             </Head>
 
             <SideBar infos={props.infos} />
@@ -17,19 +18,23 @@ let Home = props => {
 
 export async function getStaticProps() {
     const res = await fetch('https://api.github.com/users/fduquesne');
-    const user = await res.json();
+    let user;
+
+    if (res.status == 200) {
+        user = await res.json();
+    }
 
     return {
         props: {
             infos: {
-                username: user.login,
-                name: user.name,
-                bio: user.bio,
-                company: user.company,
-                location: user.location,
-                email: user.email,
-                avatar: user.avatar_url,
-                github_url: user.html_url
+                username: user && user.login ? user.login : 'fduquesne',
+                name: user && user.name ? user.name : 'Florian Duquesne',
+                bio: user && user.bio ? user.bio : '🚀DevOps & 👨🏼‍💻Front and Back-end Javascript Developer',
+                company: user && user.company ? user.company : 'Freelance',
+                location: user && user.location ? user.location : 'Lille - France',
+                email: user && user.email ? user.email : 'fduquesne@icloud.com',
+                avatar: user && user.avatar_url ? user.avatar_url : 'https://avatars1.githubusercontent.com/u/55619822?v=4',
+                github_url: user && user.html_url ? user.html_url : 'https://github.com/fduquesne'
             }
         }
     };
